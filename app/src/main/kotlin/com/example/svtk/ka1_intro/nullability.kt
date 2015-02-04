ca3862646d2fb6cdb4c5785798b9f34513a614ba
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2015-02-04 21:57:55 +0300

    nullability

---
+++ app/src/main/kotlin/com/example/svtk/ka1_intro/nullability.kt
@@ -0,0 +1,20 @@
+package com.example.svtk.ka1_intro
+
+fun foo1(): String = ""
+fun foo2(): String? = null
+
+fun testNullability() {
+    val s1: String = "always not null"
+    val s2: String? = null
+
+    val i1: Int = s1.length()
+
+    val i2: Int? = s2?.length()
+
+    val i3: Int = s2?.length() ?: 0
+
+    s2!! //throws NPE if s2 is null
+
+    if (s2 == null) return
+    s2.length()
+}
