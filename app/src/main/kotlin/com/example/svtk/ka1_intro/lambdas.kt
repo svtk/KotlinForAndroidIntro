28848e6dc0943bae6fcd41462999d9e37c2d163d
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2015-03-26 22:08:19 +0100

    Added lambdas & collections examples

--- app/src/main/kotlin/com/example/svtk/ka1_intro/lambdas.kt
+++ app/src/main/kotlin/com/example/svtk/ka1_intro/lambdas.kt
@@ -3,9 +3,9 @@ package com.example.svtk.ka1_intro
 
 fun testLambdas() {
 
-    val sum = { (x: Int, y: Int) -> x + y }
+    val sum: (Int, Int) -> Int = { (x: Int, y: Int) -> x + y }
 
-    val three = sum(1, 2)
+    sum(1, 2) == 3 //true
 
 
     fun apply(i: Int, f: (Int) -> Int) = f(i)
@@ -14,5 +14,15 @@ fun testLambdas() {
 
     //you can omit round brackets if lambda is the last argument
     apply(2) { x -> x + 25 }
+
+    val predicate: (Int) -> Boolean = { x: Int -> x > 0 }
+
+    val list = listOf(1, -1, 2)
+
+    list.filter(predicate)
+
+    list.filter { it > 0 }
+
+    list.map { "" }
 }
 
